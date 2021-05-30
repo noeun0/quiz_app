5fff55af22b389fea7a4afe1cbd1b0652e4d3335
noeun0 <61595578+noeun0@users.noreply.github.com>
2021-05-30 21:55:52 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app1/screen/screen_home.dart';
+
+// sdasdasd
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
